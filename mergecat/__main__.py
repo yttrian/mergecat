4d28c219a7043f9d4e27fbb63a07b7ee669a7c83
Ian Moore <privet.privet250@gmail.com>
2020-01-04 23:16:47 -0500

    Implement manual timing input based on earlier program
    This is essentially a copy of something I wrote before,
    in the future timing should be automatically detected.

--- mergecat/__main__.py
+++ mergecat/__main__.py
@@ -1,12 +1,39 @@
+from subprocess import call
 import click
 
 
-@click.command()
-@click.option("--input",
-              prompt="Video file to process",
-              help="Location of video file to process")
-def mergecat(video_file):
-    click.echo("Hello world!")
+@click.command(help="Given a video file and a timing file, "
+                    "a clip will be created by cutting at given timestamps and then combining.\n\n"
+                    "FFmpeg needs to be installed on your system in order to use this program.\n\n"
+                    "The timing file should be a text file with clip timestamps separated by new lines,"
+                    "in a format like this '30 70'.")
+@click.argument("video_file",
+                type=click.Path())
+@click.argument("timing_file",
+                type=click.File())
+@click.option("--out",
+              default="out.mkv",
+              help="Output file, default is out.mkv in current directory",
+              type=click.Path(exists=True, writable=True))
+def mergecat(video_file, timing_file, *, out):
+    concat = open("concat.txt", "w")
+
+    for count, timestamp in enumerate(timing_file):
+        start, end = [int(i) for i in timestamp.split(" ")]
+        duration = end - start
+
+        click.echo(f"Creating clip {count}")
+        call(["ffmpeg", "-y", "-loglevel", "panic",
+              "-i", video_file,
+              "-ss", "start", "-t", duration,
+              "-c", "copy", f"in{count}.mkv"])
+        concat.write(f'file \'in{count}.mkv\'\n')
+
+    concat.close()
+    click.echo(f'Creating final output')
+    call(["ffmpeg", "-y", "-loglevel", "panic",
+          "-f", "concat", "-i", "concat.txt",
+          "-c", "copy", out])
 
 
 if __name__ == '__main__':
